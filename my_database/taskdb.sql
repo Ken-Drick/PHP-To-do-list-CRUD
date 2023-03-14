bda045319aa0325900eec7aa554f06b521990662
Ken-Drick <120768809+Ken-Drick@users.noreply.github.com>
2023-03-14 19:43:50 +0800

    pahabol po ma'am

---
+++ my_database/taskdb.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 14, 2023 at 11:41 AM
+-- Server version: 8.0.30
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `taskdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `task_name` varchar(60) NOT NULL,
+  `task_description` varchar(100) NOT NULL,
+  `task_due_date` date NOT NULL,
+  `task_status` enum('Incomplete','In Progress','Complete') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `task_name`, `task_description`, `task_due_date`, `task_status`) VALUES
+(1, 'Records App', 'Submit Records App repository', '2023-03-14', 'In Progress'),
+(2, 'PHP CRUD', 'Push changes to repo', '2023-03-04', 'Incomplete'),
+(3, 'Requirements', 'gawin na mga requirements', '2023-03-11', 'In Progress');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
